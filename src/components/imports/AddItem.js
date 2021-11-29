4bfe043f9de91c5b67e0d26acd35585dd37cffeb
efam0820 <gajelanmarife@gmail.com>
2021-11-30 02:50:38 +0800

    Modify the design of Modal

--- src/components/imports/AddItem.js
+++ src/components/imports/AddItem.js
@@ -45,54 +45,58 @@ const AddItem = ({handleClose, show, categoryID, title, suppliers}) => {
     console.log(suppliers);
 
     return (
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header>
-                <Modal.Title className="center inputBox">Add new item ({title})</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <div className="center">
-                    <form onSubmit={onSubmit}>
-                        <div className="inputbox">
-                            <input type="text" required="required" 
-                                value={itemName}
-                                onChange={(e) => setItemName(e.target.value)}/>
-                            <span>Item name</span>
-                        </div>
+        
+            <Modal show={show} onHide={handleClose}>
+                <div className='modal_main'>
+                    <Modal.Header id='modal_header'>
+                        <Modal.Title className="center inputBox">Add new item ({title})</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body id='modal_body'>
+                        <div className="center">
+                            <form onSubmit={onSubmit}>
+                                <div className="inputbox">
+                                    <input type="text" required="required" 
+                                        value={itemName}
+                                        onChange={(e) => setItemName(e.target.value)}/>
+                                    <span>Item name</span>
+                                </div>
 
-                        <div className="inputbox">
-                            <input type="number" required="required"
-                                value={quantity} 
-                                onChange={(e) => setQuantity(e.target.value)}/>
-                            <span>Quantity</span>
-                        </div>
+                                <div className="inputbox">
+                                    <input type="number" required="required"
+                                        value={quantity} 
+                                        onChange={(e) => setQuantity(e.target.value)}/>
+                                    <span>Quantity</span>
+                                </div>
 
-                        <div className="inputbox">
-                            <input type="number" required="required"
-                                value={price} 
-                                onChange={(e) => setPrice(e.target.value)}/>
-                            <span>Price</span>
-                        </div>
+                                <div className="inputbox">
+                                    <input type="number" required="required"
+                                        value={price} 
+                                        onChange={(e) => setPrice(e.target.value)}/>
+                                    <span>Price</span>
+                                </div>
 
-                        <div className="inputbox">
-                            <select value={supplier} onChange={(e) => setSupplier(e.target.value)} id="suppliers" name="suppliers" required>
-                                <option value="" disabled selected>Select Supplier</option>
-                                { suppliers.map(supplier => 
-                                 <option key = {supplier.supplierID} value={supplier.supplierID}>
-                                     {supplier.supplierName}
-                                 </option>) }
-                            </select>
-                        </div>
+                                <div className="inputbox">
+                                    <select value={supplier} onChange={(e) => setSupplier(e.target.value)} id="suppliers" name="suppliers" required>
+                                        <option value="" disabled selected>Select Supplier</option>
+                                        { suppliers.map(supplier => 
+                                        <option key = {supplier.supplierID} value={supplier.supplierID}>
+                                            {supplier.supplierName}
+                                        </option>) }
+                                    </select>
+                                </div>
 
-                        <Modal.Footer>
-                            <Button style={{backgroundColor: '#69c79d'}}
-                                type='submit' value='Add item'>
-                                Add item
-                            </Button>
-                        </Modal.Footer>
-                    </form>
+                                <Modal.Footer id='modal_footer'>
+                                    <Button id='add_item'
+                                        type='submit' value='Add item'>
+                                        Add New Item
+                                    </Button>
+                                </Modal.Footer>
+                            </form>
+                        </div>
+                    </Modal.Body>
                 </div>
-            </Modal.Body>  
-        </Modal>
+            </Modal>
+        
     )
 }
 
